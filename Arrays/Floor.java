7fb75f0dde700ad47a7027c90ee63727d41fe989
Your Name <you@example.com>
2024-10-13 22:36:16 +0530

    code

---
+++ Arrays/Floor.java
@@ -0,0 +1,25 @@
+public class Floor {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,6,7};
+        int target=8;
+        int result=floorNumber(arr,target);
+        System.out.println(result);
+    }
+    public static int floorNumber(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target==arr[mid]){
+                return mid;
+            }
+            else if(target<arr[mid]){
+                 end=mid-1;
+            }
+            else{
+                 start=mid+1;
+            }
+        }
+        return arr[end];
+    }
+}
